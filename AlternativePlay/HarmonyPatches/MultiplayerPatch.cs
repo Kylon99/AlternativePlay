665cd00a740fd971f2e14876bd8e2ac7ee4bf0ac
Snow <egg_quall@hotmail.com>
2021-03-09 18:56:57 +0900

    Fixing declaration omissions and cleaning up the code

--- AlternativePlay/HarmonyPatches/MultiplayerPatch.cs
+++ AlternativePlay/HarmonyPatches/MultiplayerPatch.cs
@@ -3,13 +3,11 @@
 namespace AlternativePlay.HarmonyPatches
 {
     [HarmonyPatch(typeof(MultiplayerLobbyConnectionController), "HandleMultiplayerSessionManagerConnected")]
-    class MultiplayerPatch
+    internal class MultiplayerPatch
     {
         public static MultiplayerLobbyConnectionController.LobbyConnectionType connectionType;
-        static void Postfix(MultiplayerLobbyConnectionController __instance)
+        private static void Postfix(MultiplayerLobbyConnectionController __instance)
         {
-            //AlternativePlay.Logger.Notice($"Patch MultiplayerLobbyConnenctionController {__instance.connectionState.ToString()}");
-            //AlternativePlay.Logger.Notice($"Patch MultiplayerLobbyConnenctionController {__instance.connectionType.ToString()}");
             connectionType = __instance.connectionType;
         }
     }
